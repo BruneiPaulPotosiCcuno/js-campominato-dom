0565720de53d55e0a7a2e99841fd559a8a6cb76c
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-18 23:48:51 +0100

    add-cooments

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.ç
+
+//prendiamo il btn del html
+const btn = document.querySelector('#btn');
+
+//creo l'evento listener per il btn
+btn.addEventListener('click', function() {
+    //Creo la griglia per i numeri
+    const mainGrid = document.querySelector('#grid');
+    for (let i = 1; i <= 100; i++){
+        const newSquare = generateSquare(i);
+        //aggiungo square allla griglia
+        mainGrid.appendChild(newSquare);
+    }
+});
+
+//Creo la funzione per generare ogni cella della griglia
+function generateSquare(number){
+    //creo un div con lo square e il numero per ogni cella
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = '<span>' + number + '</span>';
+
+    //aggiungo la cella l'evento click 
+    newSquare.addEventListener('click', function(){
+        this.classList.add('blue'); 
+        //Ogni cella clickata viene mostrata nel cl
+        console.log("Numero cella" + number );
+    })
+    
+    //retorna 
+    return newSquare;
+}
+
+//Creo un arrary per tenere le "bombe"
+//Creo un punteggio per il giocatore
+//Function per generare le bombe
+
+//Aggiornamento della funzione
