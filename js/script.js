31afacba6b02c5e2aa4c0c8943f8a3eba6784de8
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-19 04:05:49 +0100

    add-grid-reset

--- js/script.js
+++ js/script.js
@@ -4,37 +4,7 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.ç
 
-//prendiamo il btn del html
-const btn = document.querySelector('#btn');
 
-//creo l'evento listener per il btn
-btn.addEventListener('click', function() {
-    //Creo la griglia per i numeri
-    const mainGrid = document.querySelector('#grid');
-    for (let i = 1; i <= 100; i++){
-        const newSquare = generateSquare(i);
-        //aggiungo square allla griglia
-        mainGrid.appendChild(newSquare);
-    }
-});
-
-//Creo la funzione per generare ogni cella della griglia
-function generateSquare(number){
-    //creo un div con lo square e il numero per ogni cella
-    const newSquare = document.createElement('div');
-    newSquare.classList.add('square');
-    newSquare.innerHTML = '<span>' + number + '</span>';
-
-    //aggiungo la cella l'evento click 
-    newSquare.addEventListener('click', function(){
-        this.classList.add('blue'); 
-        //Ogni cella clickata viene mostrata nel cl
-        console.log("Numero cella" + number );
-    })
-    
-    //retorna 
-    return newSquare;
-}
 
 //Creo un arrary per tenere le "bombe"
 let bombs = [];
@@ -48,5 +18,15 @@ function generateBombs(){
             bombs.push(bombPosition);
         }
     }
+    
 }
-//Aggiornamento della funzione
+//Function per il game
+
+const btn = document.querySelector('#btn');
+//*console.log(btn);
+    //aggiungo un event listener
+    btn.addEventListener('click', function() {
+        const gridMain = document.querySelector('#grid');
+        //faccio il reset della grid
+        gridMain.innerHTML = ''
+    })
